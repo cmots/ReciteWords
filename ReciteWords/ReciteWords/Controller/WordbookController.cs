0aa8a079c797635390dbed6b9edd3465c55f8fac
Unknown <1061129075@qq.com>
2019-02-28 16:59:49 +0800

    对词库的controller，改变了数据储存的相对路径

--- ReciteWords/ReciteWords/Controller/WordbookController.cs
+++ ReciteWords/ReciteWords/Controller/WordbookController.cs
@@ -13,16 +13,22 @@ namespace ReciteWords.Controller
     /// </summary>
     class WordbookController
     {
-        public Word ChooseWord(string name,int i)
+        /// <summary>
+        /// 从词库中选取一个已知序列号的单词
+        /// </summary>
+        /// <param name="name">词库名</param>
+        /// <param name="index">单词序列号</param>
+        /// <returns></returns>
+        public Word ChooseWord(string name,int index)
         {
-            string XMLFilePath = ".\\" + name + ".xml";
+            string XMLFilePath = ".\\DontTouch\\" + name + ".xml";
             XDocument doc = XDocument.Load(XMLFilePath);
             XElement root = doc.Root;
             Word word = new Word();
             try
             {
                 //XElement xItem = root.Elements("item").Where(x => (int)x.Element("progress") == -1).First();
-                XElement xItem = root.Elements("item").ElementAtOrDefault(i);
+                XElement xItem = root.Elements("item").ElementAtOrDefault(index);
                 if ((int)xItem.Element("progress") == -1)
                 {
                     word._Word = xItem.Element("word").Value;
@@ -31,7 +37,7 @@ namespace ReciteWords.Controller
                 }
                 else
                 {
-                    word._Word = "根据序列号找到了一个单词，但显示它已经被背过了";
+                    word._Word = "根据序列号找到了一个单词，但显示它已经被背过了,而这种情况是不可能出现的";
                 }
             }
             catch(Exception e)
@@ -40,6 +46,72 @@ namespace ReciteWords.Controller
                 return word;
             }
             return word;
+        }
+
+        /// <summary>
+        /// 为刚使用的词库添加序列号
+        /// </summary>
+        /// <param name="name">词库名</param>
+        /// <returns></returns>
+        public static bool MakeIndex(string name)
+        {
+            string XMLFilePath = ".\\DontTouch\\wordbook\\" + name + ".xml";
+            XDocument doc = XDocument.Load(XMLFilePath);
+            XElement root = doc.Root;
+
+            XElement xIndex = root.Element("index");
+            if(xIndex==null)
+            {
+                XElement nIndex = new XElement("index");
+                nIndex.SetAttributeValue("index", 0);
+                root.AddFirst(nIndex);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 从词库中提取出序列号
+        /// </summary>
+        /// <param name="name">词库名</param>
+        /// <returns></returns>
+        public static int GetIndex(string name)
+        {
+            string XMLFilePath = ".\\DontTouch\\wordbook\\" + name + ".xml";
+            XDocument doc = XDocument.Load(XMLFilePath);
+            XElement root = doc.Root;
+            XElement xIndex = root.Element("index");
+            if (xIndex == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return Convert.ToInt16(xIndex.Value);
+            }
+        }
+
+        /// <summary>
+        /// 将词库序列号加一
+        /// </summary>
+        /// <param name="name">词库名</param>
+        /// <returns></returns>
+        public static bool UpdateIndex(string name)
+        {
+            string XMLFilePath = ".\\DontTouch\\wordbook\\" + name + ".xml";
+            XDocument doc = XDocument.Load(XMLFilePath);
+            XElement root = doc.Root;
+            XElement xIndex = root.Element("index");
+            if (xIndex == null)
+            {
+                return false;
+            }
+            else
+            {
+                int value = Convert.ToInt16(xIndex.Value);
+                xIndex.Value = (value + 1).ToString();
+                return true;
+            }
         }
     }
 }
--- ReciteWords/ReciteWords/Form1.Designer.cs
+++ ReciteWords/ReciteWords/Form1.Designer.cs
@@ -29,23 +29,48 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(144, 340);
+            this.textBox1.Location = new System.Drawing.Point(78, 167);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(112, 85);
+            this.textBox1.Size = new System.Drawing.Size(58, 43);
             this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始背了！";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "今天要背多少单词呢";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1855, 1016);
+            this.ClientSize = new System.Drawing.Size(928, 488);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
