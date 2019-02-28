4469e3f2a8bd3d0660b5cbd9aba0a9c3d0f2fd2e
Unknown <1061129075@qq.com>
2019-02-28 09:47:59 +0800

    xmlcontroller

---
+++ ReciteWords/ReciteWords/Controller/XMLController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using ReciteWords.Model;
+
+namespace ReciteWords.Controller
+{
+    class XMLController
+    {
+        //用来初始化一个完全新的个人单词列表
+        public static bool CreateXML(string name)
+        {
+            string XMLFilePath = ".\\" + name + ".xml";
+            if (!File.Exists(XMLFilePath))
+            {
+                File.Create(XMLFilePath);
+            }
+            try
+            {
+                XDocument doc = new XDocument(
+                    new XDeclaration("1.0", "utf-8", "yes"),
+                    new XElement("wordbook")
+                        );
+                doc.Save(XMLFilePath);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        //用来在未来某个日期内添加单词
+        public static bool AddDate(string name, string date)
+        {
+            string XMLFilePath = ".\\" + name + ".xml";
+            XDocument doc = XDocument.Load(XMLFilePath);
+            XElement root = doc.Root;
+
+            XElement xDate = root.Elements("date").Where(x => x.Attribute("date").Value == date).Single();
+
+            if (xDate == null)
+            {
+                try
+                {
+                    XElement nDate = new XElement("date");
+                    nDate.SetAttributeValue("date", date);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+                return true;
+
+            }
+            else
+                return false;
+        }
+
+        //用来在某个日期单词列表内添加新的单词
+        public static bool AddWord(string name, string date, Word word)
+        {
+            string XMLFilePath = ".\\" + name + ".xml";
+            XDocument doc = XDocument.Load(XMLFilePath);
+            XElement root = doc.Root;
+
+            XElement xDate = root.Elements("date").Where(x => x.Attribute("date").Value == date).Single();
+
+            if (xDate == null)
+                return false;
+
+            try
+            {
+                XElement item = new XElement("item");
+
+                XCData trans = new XCData(word.Trans);
+                XCData phonetic = new XCData(word.Phonetic);
+
+                item.SetElementValue("word", word._Word);
+                item.SetElementValue("trans", trans);
+                item.SetElementValue("phonetic", phonetic);
+                item.SetElementValue("progress", word.Progress);
+
+                xDate.Add(item);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        } 
+    }
+}
---
+++ ReciteWords/ReciteWords/Model/word.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReciteWords.Model
+{
+    class Word
+    {
+        private string word;
+
+        private string trans;
+
+        private string phonetic;
+
+        private int progress;
+
+        public string _Word { get => word; set => word = value; }
+
+        public string Trans { get => trans; set => trans = value; }
+
+        public string Phonetic { get => phonetic; set => phonetic = value; }
+
+        public int Progress { get => progress; set => progress = value; }
+    }
+}
